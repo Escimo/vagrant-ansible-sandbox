5bfbdd80dae49b907353eb017ef9cf34b50de15b
stanislav.politikin <stanislav.politikin@fossa.pp.ua>
2023-10-04 15:13:11 +0200

    Add Lessons 8 and 9

--- Lesson-8-Lists1.py
+++ Lesson-8-Lists1.py
@@ -7,3 +7,40 @@
 #                                    #
 # ---------------------------------- #
 
+cities = ['New York', 'sumy', 'Szczecin', 'Winnipeg', 'London']
+
+print(cities)
+print(len(cities)) #Выводим количество значений(длинну массива)
+
+print(cities[0])
+print(cities[-1])
+print(cities[1].upper())
+
+#Заменяем указанное значение другим
+cities[2] = 'Stargard'
+print(cities)
+
+#Добавляем значение в конец списка
+cities.append('Kyiv')
+cities.append('Vancuver')
+print(cities)
+
+#Добавляем значение на определенную позицию
+cities.insert(1, 'Los Angeles')
+print(cities)
+
+#Удаление значения из массива
+del cities[2]
+print(cities)
+#Если не знаем номер в огромном массиве - можно использовать название
+cities.remove('Winnipeg')
+print(cities)
+
+#Удаляет последнее (если не указано другое) значение и выводит сообщение.
+deleted_city = cities.pop()
+print("Deleted city is: " + deleted_city)
+print(cities)
+
+#Сортировка
+cities.sort(reverse=False)
+print(cities)
--- Lesson-9-Lists2Cycles.py
+++ Lesson-9-Lists2Cycles.py
@@ -7,3 +7,48 @@
 #                                    #
 # ---------------------------------- #
 
+#         0      1      2       3          4
+cars = ['audi', 'vw', 'seat', 'skoda', 'porsche']
+
+for bibika in cars:
+    print(bibika.upper())
+
+"""
+print("\n----------------------------------\n")
+"""
+
+for n in range(1, 10):
+    print(n)
+
+print("\n----------------------------------\n")
+
+#Создаем массив
+mynumber_list = list(range(0, 10))
+print(mynumber_list) #просто посмотрим результат
+print("================================")
+for x in mynumber_list:
+    x = x * 2
+    print(x)
+
+mynumber_list.sort(reverse=True)
+print(mynumber_list)
+
+print("Max number is: " + str(max(mynumber_list)))
+print("Max number is: " + str(min(mynumber_list)))
+print("Sum of list is: " + str(sum(mynumber_list)))
+
+print("\n----------------------------------\n")
+#         0      1      2       3          4
+cars = ['audi', 'vw', 'seat', 'skoda', 'porsche']
+
+mycars = cars[1:4] #считаем от 1 до 4 (4 НЕ ВКЛЮЧИТЕЛЬНО)
+print(mycars)
+mycars = cars[:3] #Считаем с самого начала (0) до  3
+print(mycars)
+mycars = cars[-3:-1] #Считаем в обратную сторону
+
+print("\n----------------------------------\n")
+#Скопировать массив
+#         0      1      2       3          4
+cars = ['audi', 'vw', 'seat', 'skoda', 'porsche']
+mycars = cars[:] # С [:] копируем cars и даже после изменения cars массив mycars не изменится
